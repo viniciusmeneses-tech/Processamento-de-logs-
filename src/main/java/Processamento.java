22d4e6f11d5f5adcc96f0a84c6741f7a5d8ec90d
yan-dhsk <yanvianaaa@gmail.com>
2026-05-04 19:20:14 -0300

    fix: Correção de bugs e erros das opção 1 e 2 da classe processamento
    - Alterei os valores das variáveis 'dataantes' e 'datadepois' para garantir filtrar por novembro (antes pegava outubro)
    - Corrige lista imutavel List.of para ArrayList (ia dar erro no .add)
    - Corrige concatenacao de strings nos dados processados (faltou os espaços ' ')
    - Coloquei o construtor como publico (a main não ia conseguir chamar)

--- src/main/java/Processamento.java
+++ src/main/java/Processamento.java
@@ -1,22 +1,23 @@
 import java.util.List;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Processamento{
 	
 	private List<Dados> dadosbrutos;
 	
-	private Processamento(List<Dados> dadosbrutos){
+	public Processamento(List<Dados> dadosbrutos){
 		this.dadosbrutos = dadosbrutos;
 	}
 	
 	
 	public void opcaoUm() {
-		List<String> dadosprocessados = List.of();
+		List<String> dadosprocessados = new ArrayList<>();
 		for(int n=0; n<dadosbrutos.size();n++){
 			int codigo=dadosbrutos.get(n).getcodigoResposta();
 			int tamanho=dadosbrutos.get(n).gettamanhoObj();
 			if( codigo >= 200 && codigo <= 299 && tamanho > 2000){
-				String dado = (dadosbrutos.get(n).getcodigoResposta())+(dadosbrutos.get(n).gettamanhoObj())+(dadosbrutos.get(n).getIP());
+				String dado = (dadosbrutos.get(n).getcodigoResposta())+ " " + (dadosbrutos.get(n).gettamanhoObj())+ " " + (dadosbrutos.get(n).getIP());
 				dadosprocessados.add(dado);
 			}
 		}
@@ -25,20 +26,25 @@ public class Processamento{
 	}
 	
 	public void opcaoDois() {
-		List<String> dadosprocessados = List.of();
-		LocalDateTime dataantes=LocalDateTime.of(2021, 9, 31, 23, 59);
-		LocalDateTime datadepois=LocalDateTime.of(2021, 11, 1, 00, 00);
+		List<String> dadosprocessados = new ArrayList<>();
+		LocalDateTime dataantes=LocalDateTime.of(2021, 10, 31, 23, 59, 59);
+		LocalDateTime datadepois=LocalDateTime.of(2021, 12, 1, 00, 00, 00);
 		for(int n=0; n<dadosbrutos.size();n++){
 			int codigo=dadosbrutos.get(n).getcodigoResposta();
 			String ref=dadosbrutos.get(n).getreferencia();
 			LocalDateTime data=dadosbrutos.get(n).getdata();
 			if( codigo >= 400 && codigo <= 499 && data.isAfter(dataantes) && data.isBefore(datadepois)){
-				String dado = (codigo)+(ref)+"Nov/21";
+				String dado = (codigo) + " \"" +(ref)+ "\" " +"Nov/2021";
 				dadosprocessados.add(dado);
 			}
 		}
 		Escrita escrita = new Escrita();
 		escrita.salvarArquivo("naoRespondidosNovembro.txt", dadosprocessados);
+	}
+	
+	public void opcaoQuatro() {
+		
+		
 	}
 }
 
